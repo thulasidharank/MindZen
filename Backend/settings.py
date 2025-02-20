af96db7cb4f9c33a8debdf1444b7143c6c30a652
Ashique <ashique9496@gmail.com>
2025-02-20 17:29:31 +0530

    first commit

--- Backend/settings.py
+++ Backend/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-1f6ouvwdt7)mbk(e9j4&s6avx4psrd26887y)(c&l_fse(5_@t'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
