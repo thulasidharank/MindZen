b8bf42e92e71d93dc94508e1302ace331be634b6
Ashique <ashique9496@gmail.com>
2025-03-21 15:00:03 +0530

    started to created each service page

--- Home/views.py
+++ Home/views.py
@@ -2,3 +2,6 @@ from django.shortcuts import render
 
 def home(request):
     return render(request, 'index.html')
+
+def staff_augmentation(request):
+    return render(request, 'staff_augmentation.html')
